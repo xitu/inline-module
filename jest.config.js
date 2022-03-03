7a0c908e74b1d8908a9825e1b25269f636a93747
Flame <wshx1938@163.com>
2022-03-03 22:52:32 +0800

    test: add test cases

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    preset: 'jest-puppeteer',
+    globals: {
+        URL: 'http://localhost:8080',
+    },
+    verbose: true,
+};
---
+++ tests/index.spec.js
@@ -0,0 +1,24 @@
+const timeout = 5000;
+const fs = require('fs');
+const path = require('path');
+
+async function getDocCase(file) {
+    let filePath = fs.realpathSync(path.join(__dirname, file));
+    await page.goto('file://' + filePath, { waitUntil: 'domcontentloaded' });
+}
+
+describe('inline-module', () => {
+    test(
+        'importing inline module correctly',
+        async () => {
+            await getDocCase('./pages/simple.html');
+            const value = await page.evaluate(() => window.fooValue);
+            expect(value).toBe('bar');
+        },
+        timeout
+    );
+
+    test('more tests...', () => {
+        expect(1).toBe(1);
+    });
+});
