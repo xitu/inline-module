77ed62b1abb664f4b1b36ab4962dae4cabeb8053
akira-cn <19511344@qq.com>
2022-03-07 12:25:35 +0800

    fixed ts filename

--- loaders/typescript-loader.js
+++ loaders/typescript-loader.js
@@ -3,7 +3,10 @@ import { requireBabel } from "./utils";
 export default {
   transform(code, options = {}) {
     const {availablePresets, transform} = requireBabel();
-    options.filename = `${options.filename}.ts`;
+    const {filename} = options;
+    if(!/\.ts$/.test(filename)) {
+      options.filename = `${options.filename}.ts`;
+    }
     return transform(code, {
       presets: [availablePresets.typescript],
       ...options
